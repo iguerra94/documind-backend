4341e39472112e0811a2428ae6c369fe2f09f07e
Ivan Gabriel Guerra <ivanguerra09@gmail.com>
2026-04-02 20:38:56 -0300

    chore: bootstrap Spring Boot project

---
+++ src/main/java/com/ivan/documind/documind_backend/DocumindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.documind.documind_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumindBackendApplication.class, args);
+	}
+
+}
