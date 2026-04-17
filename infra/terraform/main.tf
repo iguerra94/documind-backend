1a047a2c1724ef70997491baa0e7aa91ae57332a
Ivan Gabriel Guerra <ivanguerra09@gmail.com>
2026-04-17 16:52:06 -0300

    chore: pass observability metadata to ecs task

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -111,7 +111,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.main.id
   }
 
@@ -339,6 +339,14 @@ resource "aws_ecs_task_definition" "app" {
         {
           name  = "SPRING_PROFILES_ACTIVE"
           value = "prod"
+        },
+        {
+          name  = "APP_ENVIRONMENT"
+          value = "prod"
+        },
+        {
+          name  = "APP_RELEASE"
+          value = var.image_tag
         }
       ]
 
